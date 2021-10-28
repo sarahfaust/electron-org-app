c3a22a648da0c9baedba9073e2fb3a5c61d14198
sarah faust <sarah.faustmann@gmail.com>
2021-10-28 17:17:11 +0200

    Set up project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+const App = () => (
+  <div>
+    This is your shiny new first desktop application! Now on to{' '}
+    <strong>user authentication</strong>.
+  </div>
+);
+
+export default App;
